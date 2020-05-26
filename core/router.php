55d6f66fc0489c2af3a400dd3a994239671e3eb6
adazure <aske2005@hotmail.com>
2020-05-27 02:05:21 +0300

    Updated Twig

--- core/router.php
+++ core/router.php
@@ -1,14 +1,6 @@
 <?php
 if (!empty($ROUTES)) {
 
-    $twig;
-    $loader = new Twig_Loader_Filesystem('views');
-    $this->twig = new Twig_Environment($loader);
-    $filter = new Twig_Filter('lang', 'Lang::get');
-    $this->twig->addFilter($filter);
-    $this->twig->addFunction(new Twig_Function('query', 'Router::getQuery'));
-    $this->twig->addFilter(new Twig_Filter('html', 'htmlspecialchars_decode'));
-
     // Request üzerinden gelen bir takım değerler alınıyor
     $REQUEST_METHOD = $_SERVER['REQUEST_METHOD'];
     $REQUEST_URI = $_SERVER['REQUEST_URI'];
@@ -25,7 +17,7 @@ if (!empty($ROUTES)) {
         // Regex ile karşılaştırma yapılıyor
         preg_match_all('/^' . $regex . '[\/]*$/', $REQUEST_URI, $result);
         if (count($result[0]) > 0) {
-            echo file_get_contents('views/' . $view);
+            echo $twig->render($view . '.html', []);
             exit;
         }
     }
